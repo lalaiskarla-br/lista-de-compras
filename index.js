64d4ccfaf416eab2564431ebfc6d69bb7d2ba81c
Laís Barros <lalaiskarla@gmail.com>
2026-04-08 23:43:16 -0300

    Insere primeira variavel javascript

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
- function monstrarMensagem(){
-        alert('Item salvo')
-      }
+function monstrarMensagem() {
+  alert("Item salvo");
+}
+
+const inputItem = document.getElementById("input-item");
+
+console.log(inputItem); 
+
+/* console.log(inputItem.value);
+com value não irá retornar informação, pois serão necessários ouvintes de eventos;*/ 
