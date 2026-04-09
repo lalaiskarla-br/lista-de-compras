8f584de79ea2c4409a9d2917b148b32efd0632e1
Laís Barros <lalaiskarla@gmail.com>
2026-04-08 23:48:28 -0300

    Insere anotacoes sobre a como retornar o value

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ console.log(inputItem);
 
 /* console.log(inputItem.value);
 com value não irá retornar informação, pois serão necessários ouvintes de eventos;*/ 
+
+/* também seria possível escrever- atinge o mesmo objetivo que é capturar o valor e mostrar na tela:
+
+const item = document.getElementById("input-item").value;
+console.log(item);
+
+*/
