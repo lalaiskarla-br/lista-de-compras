9d3288fe94691df8fde7c7d160baacdc5b88ad71
Laís Barros <lalaiskarla@gmail.com>
2026-04-13 23:27:35 -0300

    Adiciona itens a lista com JS

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function monstrarMensagem() {
+/* function monstrarMensagem() {
   alert("Item salvo");
 }
 
 const inputItem = document.getElementById("input-item");
 
-console.log(inputItem); 
+console.log(inputItem); */ 
+
+
+/*const tecnologia = document.getElementsBy("input-item");
+
+
+console.log("Estou estudando" tecnologia); */
+
+
 
 /* console.log(inputItem.value);
 com value não irá retornar informação, pois serão necessários ouvintes de eventos;*/ 
@@ -15,3 +23,38 @@ const item = document.getElementById("input-item").value;
 console.log(item);
 
 */
+
+
+//alert("Integração realizada com sucesso!")
+
+const inputItem = document.getElementById("input-item");
+const botaoAdicionar = document.getElementById("adicionar-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+let contador = 0;
+
+  botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if (inputItem.value === "") {
+      alert("Por favor, insira um item!");
+      return
+    }
+    
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
+
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+   containerItemDaLista.appendChild(inputCheckbox);
+   containerItemDaLista.appendChild(nomeItem);
+   itemDaLista.appendChild(containerItemDaLista);
+   listaDeCompras.appendChild(itemDaLista);
+
+
+  })
+
